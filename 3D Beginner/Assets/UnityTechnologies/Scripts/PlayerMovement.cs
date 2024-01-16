7386ed7f128b9dddf8a54397cf516bc6156ee335
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2024-01-16 12:55:42 -0800

    Did thingies :3

--- 3D Beginner/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ 3D Beginner/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector3 m_Movement; //m = Member Variable
+   public float turnSpeed = 20f;
+
     Animator m_Animator;
-    public float turnSpeed = 20f;
-      Rigidbody m_Rigidbody;
-       Quaternion m_Rotation = Quaternion.identity;
-    void Start()
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement; //Member Value
+    Quaternion m_Rotation = Quaternion.identity;
+
+    void Start ()
     {
-     m_Animator = GetComponent<Animator>();
+        m_Animator = GetComponent<Animator> ();
+        m_Rigidbody = GetComponent<Rigidbody> ();
     }
 
-    void Update()
+    void FixedUpdate ()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-         
-         
-         m_Movement.Set(horizontal, 0f, vertical); //Can be set and get "Setting the stage"
-          m_Movement.Normalize (); //Rounds off the values
-            Quaternion m_Rotation = Quaternion.identity;
-
+        float horizontal = Input.GetAxis ("Horizontal");
+        float vertical = Input.GetAxis ("Vertical");
+        
+         m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize ();
 
         bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
-        m_Animator.SetBool("isWalking", isWalking);
+        m_Animator.SetBool ("isWalking", isWalking);
 
-         Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation (desiredForward);
     }
 
