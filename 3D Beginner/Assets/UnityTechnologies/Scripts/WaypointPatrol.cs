af52586839d9527ecf2752085936fc13277360b0
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2024-01-22 12:37:52 -0800

    Update WaypointPatrol.cs

--- 3D Beginner/Assets/UnityTechnologies/Scripts/WaypointPatrol.cs
+++ 3D Beginner/Assets/UnityTechnologies/Scripts/WaypointPatrol.cs
@@ -7,20 +7,21 @@ public class WaypointPatrol : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
+
     int m_CurrentWaypointIndex;
 
     void Start()
     {
-        navMeshAgent.SetDestination(waypoints[0].position); //set the initial destination of the Nav Mesh Agent
+        navMeshAgent.SetDestination(waypoints[0].position);
     }
 
     void Update()
     {
-        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance) //you want to know if the Nav Mesh Agent has arrived at its destination. An easy way to check this is to see whether the remaining distance to the destination is less than the stopping distance you set in the Inspector window earlier.  
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
         {
             m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
-
             navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
         }
     }
 }
+
